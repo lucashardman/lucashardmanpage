b778d603e38aaf517235b018d4b38ca4c81dada9
Lucas Hardman <lucas.hardman92@gmail.com>
2023-06-10 15:54:04 -0300

    ais um teste com wsgi

--- stock-tracker/tracker/views.py
+++ stock-tracker/tracker/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return render(request, 'tracker/index.html')
+    return render(request, 'stock-tracker/tracker/index.html')
