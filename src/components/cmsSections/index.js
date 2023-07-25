0fe6aa9c49528193a642632290d42c2a6c7d00f0
Lucas Hardman <lucas.hardman92@gmail.com>
2023-07-24 21:15:50 -0300

    Adicionando componentes da pagina labs e preparando a refatoração para usar sass

--- src/components/cmsSections/index.js
+++ src/components/cmsSections/index.js
@@ -7,6 +7,7 @@ import { SkillsBlock } from "./SkillsBlock"
 import { ContactMeBlock } from "./ContactMeBlock"
 import { PaginaEmContrucaoBlock } from "./PaginaEmContrucaoBlock"
 import { ProjectDescriptionBlock } from "./ProjectDescriptionBlock"
+import { PagelabIntroBlock } from "./PagelabIntroBlock"
 
 export const cmsSections = {
     PageportfolioProjectRecord: ProjectsBlock,
@@ -17,5 +18,6 @@ export const cmsSections = {
     CommonContactMeRecord: ContactMeBlock,
     CommonFooterRecord: FooterBlock,
     PagepaginaemconstrucaoMensagemRecord: PaginaEmContrucaoBlock,
-    PageprojectProjectRecord: ProjectDescriptionBlock
+    PageprojectProjectRecord: ProjectDescriptionBlock,
+    PagelabIntroRecord: PagelabIntroBlock
 }
